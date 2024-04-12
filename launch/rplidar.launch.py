b095dbcbb8dc87747ddd5e538e6cf1a74a4d2b8c
derLoosi <dloos.westheim@gmail.com>
2024-04-12 16:55:41 +0200

    lidar

--- launch/rplidar.launch.py
+++ launch/rplidar.launch.py
@@ -12,6 +12,7 @@ def generate_launch_description():
             output='screen',
             parameters=[{
                 'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.3:1.0-port0',
+                'serial_baudrate' : 115200,
                 'frame_id': 'laser_frame',
                 'angle_compensate': True,
                 'scan_mode': 'Standard'
